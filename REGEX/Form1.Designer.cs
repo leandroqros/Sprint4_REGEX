a3c23cfdeffe1d4a28696f017f2aecf40db86c70
Leandro Queiroz <leandroqros@gmail.com>
2022-09-30 16:18:49 +0200

    Agregar archivos de proyecto.

---
+++ REGEX/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace REGEX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_movil = new System.Windows.Forms.Label();
+            this.lb_mail = new System.Windows.Forms.Label();
+            this.lb_SW = new System.Windows.Forms.Label();
+            this.tbox_movil = new System.Windows.Forms.TextBox();
+            this.tbox_mail = new System.Windows.Forms.TextBox();
+            this.tbox_SW = new System.Windows.Forms.TextBox();
+            this.bnt_movil = new System.Windows.Forms.Button();
+            this.bnt_mail = new System.Windows.Forms.Button();
+            this.bnt_SW = new System.Windows.Forms.Button();
+            this.tbox_movil_y = new System.Windows.Forms.TextBox();
+            this.tbox_mail_y = new System.Windows.Forms.TextBox();
+            this.tbox_SW_y = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lb_movil
+            // 
+            this.lb_movil.AutoSize = true;
+            this.lb_movil.Location = new System.Drawing.Point(42, 44);
+            this.lb_movil.Name = "lb_movil";
+            this.lb_movil.Size = new System.Drawing.Size(79, 13);
+            this.lb_movil.TabIndex = 0;
+            this.lb_movil.Text = "Telefono móvil:";
+            // 
+            // lb_mail
+            // 
+            this.lb_mail.AutoSize = true;
+            this.lb_mail.Location = new System.Drawing.Point(84, 80);
+            this.lb_mail.Name = "lb_mail";
+            this.lb_mail.Size = new System.Drawing.Size(37, 13);
+            this.lb_mail.TabIndex = 1;
+            this.lb_mail.Text = "e-mail:";
+            // 
+            // lb_SW
+            // 
+            this.lb_SW.AutoSize = true;
+            this.lb_SW.Location = new System.Drawing.Point(40, 115);
+            this.lb_SW.Name = "lb_SW";
+            this.lb_SW.Size = new System.Drawing.Size(81, 13);
+            this.lb_SW.TabIndex = 2;
+            this.lb_SW.Text = "Codi Star Wars:";
+            // 
+            // tbox_movil
+            // 
+            this.tbox_movil.Location = new System.Drawing.Point(127, 41);
+            this.tbox_movil.Name = "tbox_movil";
+            this.tbox_movil.Size = new System.Drawing.Size(187, 20);
+            this.tbox_movil.TabIndex = 3;
+            // 
+            // tbox_mail
+            // 
+            this.tbox_mail.Location = new System.Drawing.Point(127, 77);
+            this.tbox_mail.Name = "tbox_mail";
+            this.tbox_mail.Size = new System.Drawing.Size(187, 20);
+            this.tbox_mail.TabIndex = 4;
+            // 
+            // tbox_SW
+            // 
+            this.tbox_SW.Location = new System.Drawing.Point(127, 112);
+            this.tbox_SW.Name = "tbox_SW";
+            this.tbox_SW.Size = new System.Drawing.Size(187, 20);
+            this.tbox_SW.TabIndex = 5;
+            // 
+            // bnt_movil
+            // 
+            this.bnt_movil.Location = new System.Drawing.Point(341, 39);
+            this.bnt_movil.Name = "bnt_movil";
+            this.bnt_movil.Size = new System.Drawing.Size(75, 23);
+            this.bnt_movil.TabIndex = 6;
+            this.bnt_movil.Text = "Verificar";
+            this.bnt_movil.UseVisualStyleBackColor = true;
+            // 
+            // bnt_mail
+            // 
+            this.bnt_mail.Location = new System.Drawing.Point(341, 75);
+            this.bnt_mail.Name = "bnt_mail";
+            this.bnt_mail.Size = new System.Drawing.Size(75, 23);
+            this.bnt_mail.TabIndex = 7;
+            this.bnt_mail.Text = "Verificar";
+            this.bnt_mail.UseVisualStyleBackColor = true;
+            // 
+            // bnt_SW
+            // 
+            this.bnt_SW.Location = new System.Drawing.Point(341, 110);
+            this.bnt_SW.Name = "bnt_SW";
+            this.bnt_SW.Size = new System.Drawing.Size(75, 23);
+            this.bnt_SW.TabIndex = 8;
+            this.bnt_SW.Text = "Verificar";
+            this.bnt_SW.UseVisualStyleBackColor = true;
+            // 
+            // tbox_movil_y
+            // 
+            this.tbox_movil_y.BackColor = System.Drawing.SystemColors.Info;
+            this.tbox_movil_y.Location = new System.Drawing.Point(434, 41);
+            this.tbox_movil_y.Name = "tbox_movil_y";
+            this.tbox_movil_y.Size = new System.Drawing.Size(354, 20);
+            this.tbox_movil_y.TabIndex = 9;
+            // 
+            // tbox_mail_y
+            // 
+            this.tbox_mail_y.BackColor = System.Drawing.SystemColors.Info;
+            this.tbox_mail_y.Location = new System.Drawing.Point(434, 77);
+            this.tbox_mail_y.Name = "tbox_mail_y";
+            this.tbox_mail_y.Size = new System.Drawing.Size(354, 20);
+            this.tbox_mail_y.TabIndex = 10;
+            // 
+            // tbox_SW_y
+            // 
+            this.tbox_SW_y.BackColor = System.Drawing.SystemColors.Info;
+            this.tbox_SW_y.Location = new System.Drawing.Point(434, 112);
+            this.tbox_SW_y.Name = "tbox_SW_y";
+            this.tbox_SW_y.Size = new System.Drawing.Size(354, 20);
+            this.tbox_SW_y.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 172);
+            this.Controls.Add(this.tbox_SW_y);
+            this.Controls.Add(this.tbox_mail_y);
+            this.Controls.Add(this.tbox_movil_y);
+            this.Controls.Add(this.bnt_SW);
+            this.Controls.Add(this.bnt_mail);
+            this.Controls.Add(this.bnt_movil);
+            this.Controls.Add(this.tbox_SW);
+            this.Controls.Add(this.tbox_mail);
+            this.Controls.Add(this.tbox_movil);
+            this.Controls.Add(this.lb_SW);
+            this.Controls.Add(this.lb_mail);
+            this.Controls.Add(this.lb_movil);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_movil;
+        private System.Windows.Forms.Label lb_mail;
+        private System.Windows.Forms.Label lb_SW;
+        private System.Windows.Forms.TextBox tbox_movil;
+        private System.Windows.Forms.TextBox tbox_mail;
+        private System.Windows.Forms.TextBox tbox_SW;
+        private System.Windows.Forms.Button bnt_movil;
+        private System.Windows.Forms.Button bnt_mail;
+        private System.Windows.Forms.Button bnt_SW;
+        private System.Windows.Forms.TextBox tbox_movil_y;
+        private System.Windows.Forms.TextBox tbox_mail_y;
+        private System.Windows.Forms.TextBox tbox_SW_y;
+    }
+}
+
